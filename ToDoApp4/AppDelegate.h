522f268d9e52c5aea71accf54adb23463d56d05b
Mike North <northm@yahoo-inc.com>
2014-01-19 22:43:10 -0800

    Initial Commit

---
+++ ToDoApp4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp4
+//
+//  Created by Mike North on 1/19/14.
+//  Copyright (c) 2014 Mike North. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoApp4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoApp4
+//
+//  Created by Mike North on 1/19/14.
+//  Copyright (c) 2014 Mike North. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
